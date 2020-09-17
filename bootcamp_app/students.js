78620ab0dacd21919bb4c5149360071f07a6753a
Mark Baker <mrbaker1917@gmail.com>
2020-09-16 19:39:20 -0700

    fixed SQL injection risk with paramaterized queries

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -6,19 +6,21 @@ const pool = new Pool({
   host: 'localhost',
   database: 'bootcampx'
 });
-const cohort_month = process.argv[2];
-const rows_limit = process.argv[3];
-
-pool.query(`
+const cohortMonth = process.argv[2];
+const rowsLimit = process.argv[3];
+const values = [`%${cohortMonth}%`, rowsLimit];
+const queryString = `
 SELECT students.id, students.name, cohorts.name as cohort_name
 FROM cohorts
 JOIN students ON (cohorts.id = students.cohort_id)
-WHERE cohorts.name LIKE '%${cohort_month}%'
-LIMIT ${rows_limit};`)
-.then(res => {
-  res.rows.forEach(user => {
-    console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort_name} cohort.`);
+WHERE cohorts.name LIKE $1
+LIMIT $2;`;
+
+pool.query(queryString, values)
+  .then(res => {
+    res.rows.forEach(user => {
+      console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort_name} cohort.`);
+    });
+    pool.end();
   })
-  pool.end();
-})
-.catch(err => console.error('query error', err.stack));
+  .catch(err => console.error('query error', err.stack));
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -6,21 +6,22 @@ const pool = new Pool({
   host: 'localhost',
   database: 'bootcampx'
 });
-const cohort_month = process.argv[2];
-
-pool.query(`
+const cohortMonth = process.argv[2];
+const values = [`%${cohortMonth}%`];
+const queryString = `
 SELECT DISTINCT teachers.name AS teacher, cohorts.name AS cohort 
 FROM teachers
 JOIN assistance_requests ON (teacher_id = teachers.id)
 JOIN students ON (student_id = students.id)
 JOIN cohorts ON (cohort_id = cohorts.id)
-WHERE cohorts.name = '${cohort_month}'
+WHERE cohorts.name LIKE $1
 ORDER BY teachers.name;
-`)
+`;
+pool.query(queryString, values)
   .then(res => {
     res.rows.forEach(a => {
       console.log(`${a.cohort}: ${a.teacher}`);
-    })
+    });
     pool.end();
   })
   .catch(err => console.error('query error', err.stack));
